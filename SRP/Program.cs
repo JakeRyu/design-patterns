33cea3867dd2a20fc02834154a9717a94b36f6e3
Jake Ryu <jake.ryu@hotmail.co.uk>
2017-09-15 18:09:19 +0100

    add single responsibility principal

---
+++ SRP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SRP
+{
+    class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void Remove(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString(){
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("I am sad");
+            journal.AddEntry("I ate a bug");
+
+            System.Console.WriteLine(journal);
+        }
+    }
+}
