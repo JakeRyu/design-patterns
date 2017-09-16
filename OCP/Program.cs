345b34510b4ecb15c999b3bf0e7ac70f27d56d6b
Jake Ryu <jake.ryu@hotmail.co.uk>
2017-09-16 10:11:08 +0100

    modifying filter breaks open close principal

--- OCP/Program.cs
+++ OCP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 namespace OCP
@@ -36,11 +37,23 @@ namespace OCP
     {
         public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
         {
-            foreach(var product in products)
-            {
-                if(product.Size == size)
-                    yield return product;
-            }
+            // foreach(var product in products)
+            // {
+            //     if(product.Size == size)
+            //         yield return product;
+            // }
+
+            return products.Where(p => p.Size == size).AsEnumerable();
+        }
+
+        public IEnumerable<Product> FilterByColour(IEnumerable<Product> products, Colour colour)
+        {
+            return products.Where(p => p.Colour == colour);
+        }
+
+        public IEnumerable<Product> FilterByColourAndSize(IEnumerable<Product> products, Colour colour, Size size)
+        {
+            return products.Where(p => p.Colour == colour && p.Size == size);
         }
     }
 
@@ -61,6 +74,17 @@ namespace OCP
             {
                 WriteLine($" - {p.Name} is large");
             }
+
+            foreach(var p in pf.FilterByColour(products, Colour.Green))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+
+            foreach(var p in pf.FilterByColourAndSize(products, Colour.Green, Size.Large))
+            {
+                WriteLine($" - {p.Name} is green and large");
+            }
+                
         }
     }
 }
